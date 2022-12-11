bdc8bbaf63bd62edece2ef8dcefa30e741b62f8e
Patrick Almeida <patricksalmeida@gmail.com>
2022-12-11 11:59:13 -0300

    feat: add node-1 to cluster

--- 02-creating-k8s-cluster/terraform.tfvars
+++ 02-creating-k8s-cluster/terraform.tfvars
@@ -1,3 +1,6 @@
 prefix = "fullcycle"
 cluster_name = "cursofc"
 logs_retention_in_days = 30
+node_desired_size = 2
+node_max_size = 4
+node_min_size = 2
--- 02-creating-k8s-cluster/variables.tf
+++ 02-creating-k8s-cluster/variables.tf
@@ -1,3 +1,6 @@
 variable "prefix" {}
 variable "cluster_name" {}
 variable "logs_retention_in_days" {}
+variable "node_desired_size" {}
+variable "node_max_size" {}
+variable "node_min_size" {}
