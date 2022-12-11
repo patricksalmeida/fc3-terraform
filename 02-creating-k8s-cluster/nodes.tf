6f21259036611f7693f8377c4798e2d84d84c278
Patrick Almeida <patricksalmeida@gmail.com>
2022-12-11 12:07:32 -0300

    feat: add node-2 and set instance types

--- 02-creating-k8s-cluster/nodes.tf
+++ 02-creating-k8s-cluster/nodes.tf
@@ -37,6 +37,29 @@ resource "aws_eks_node_group" "node-1" {
   node_role_arn = aws_iam_role.node.arn
   subnet_ids = aws_subnet.subnets[*].id
 
+  instance_types = [ "t3.micro" ]
+
+  scaling_config {
+    desired_size = var.node_desired_size
+    max_size = var.node_max_size
+    min_size = var.node_min_size
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.node-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.node-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.node-AmazonEC2ContainerRegistryReadOnly
+  ]
+}
+
+resource "aws_eks_node_group" "node-2" {
+  cluster_name = aws_eks_cluster.cluster.name
+  node_group_name = "node-2"
+  node_role_arn = aws_iam_role.node.arn
+  subnet_ids = aws_subnet.subnets[*].id
+
+  instance_types = [ "t2.micro" ]
+
   scaling_config {
     desired_size = var.node_desired_size
     max_size = var.node_max_size
